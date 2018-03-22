62efa8833490aa2a872d8eef5cc8189d5fcee046
kiddynomite71 <foster.roland@gmail.com>
2018-03-22 00:18:47 -0500

    Connect Activities to DB

--- src/app/pages/activities/activities/activities.component.ts
+++ src/app/pages/activities/activities/activities.component.ts
@@ -1,4 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { AppSettings } from '../../../app.settings'
+import { Settings } from '../../../app.settings.model'
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore'; 
+import { Observable } from 'rxjs/Observable';
+import { AngularFireModule } from 'angularfire2';
+
+
+export interface Activity {
+  description: string;
+  name: string;
+  requirements: boolean;
+}
 
 @Component({
   selector: 'app-activities',
@@ -7,7 +19,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivitiesComponent implements OnInit {
 
-  constructor() { }
+  activitiesObservableRef: AngularFirestoreCollection<Activity>;
+  activities$: Observable<Activity[]>;
+
+  public icons = ['home', 'person', 'alarm', 'work', 'mail', 'favorite']
+  public colors = ['accent', 'primary', 'warn']
+  public settings: Settings
+  constructor(private afs: AngularFirestore, public appSettings:AppSettings) { 
+    this.activitiesObservableRef = this.afs.collection<Activity>('activities');
+    this.activities$ = this.activitiesObservableRef.valueChanges();
+    this.settings = this.appSettings.settings; 
+   }
 
   ngOnInit() {
   }
