07cd7133f9db567c93355b3a8adce47873fdff4a
kiddynomite71 <foster.roland@gmail.com>
2018-03-22 00:56:09 -0500

    Update to the Camput Builder

--- src/app/pages/campsite/campout-builder/campout-builder.component.ts
+++ src/app/pages/campsite/campout-builder/campout-builder.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 /**
  * @title Stepper overview
@@ -24,4 +25,23 @@ export class CampoutBuilderComponent {
       secondCtrl: ['', Validators.required]
     });
   }
+
+    //Datepicker start date
+    startDate = new Date(1990, 0, 1);
+
+    //Datepicker with min & max validation
+    minDate = new Date(2010, 0, 1);
+    maxDate = new Date(2020, 0, 1);
+  
+    //Datepicker with filter validation
+    myFilter = (d: Date): boolean => {
+      const day = d.getDay();
+      return day !== 0 && day !== 6;
+    }
+  
+    //Datepicker input and change events
+    events: string[] = [];
+    addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
+      this.events.push(`${type}: ${event.value}`);
+    }
 }
